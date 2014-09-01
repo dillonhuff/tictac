e6c04c72192dfb3390d7b727cff2b22765ddea58
Dillon Huff <dillonhuff@gmail.com>
2014-09-01 18:11:56 -0500

    crossover appears to work

--- ttt.el
+++ ttt.el
@@ -5,6 +5,10 @@
 (defun one-of (set)
   (random-elt set))
 
+(defun one-of-with-index (set)
+  (let ((rand-ind (random (length set))))
+    (cons (elt set rand-ind) (cons rand-ind nil))))
+
 (defun other-team (team)
   (cond ((eq team 'O) 'X)
 	((eq team 'X) 'O)
@@ -161,30 +165,85 @@
 ;; Type system
 (defvar *positions* '(0 1 2 3 4 5 6 7 8 9))
 
-(defvar *expr-types*
-  '((if . ((bool position position) position))
-    (empty? . ((board position) bool))
-    (friendly? . ((board position team) bool))
-    (enemy? . ((board position team) bool))
-    (other-team . ((team) team))))
+(defvar *func-types*
+  '((if . (position bool position position))
+    (empty? . (bool board position))
+    (friendly? . (bool board position team))
+    (enemy? . (bool board position team))
+    (other-team . (team team))))
 
 (defun atom-type (a)
   (cond ((find a *positions*) 'position)
 	((eq a 'board) 'board)
 	((eq a 'team) 'team)
-	(T nil)))
+	(T (type-signature a))))
 
 (defun type-signature (comp-expr)
-  (cdr (assoc comp-expr *expr-types*)))
+  (cadr (assoc comp-expr *func-types*)))
 
 (defun compound-type (expr)
   (let* ((type-sig (type-signature (car expr)))
-	(arg-types (car type-sig))
-	(ret-type (cadr type-sig)))
+	(arg-types (cdr type-sig))
+	(ret-type (car type-sig)))
     (if (every #'identity (mapcar #'eq arg-types (map 'list #'expr-type (cdr expr))))
 	ret-type
       nil)))
+
+(defun compound-contains-type (desired-type expr)
+  (if (eq (expr-type expr) desired-type)
+      T
+    (some #'identity (mapcar (lambda (epr) (contains-type desired-type epr)) expr))))
+
+(defun contains-type (desired-type expr)
+  (cond ((atom expr) (eq (expr-type expr) desired-type))
+	(T (compound-contains-type desired-type expr))))
 	
 (defun expr-type (expr)
   (cond ((atom expr) (atom-type expr))
 	(T (compound-type expr))))
+
+;; Crossover reproduction
+(defun rand-code-point (code)
+  (if (atom code) 
+      (cons nil (cons code nil))
+      (if (= 0 (random 10))
+	  (cons nil (cons code nil))
+	(let* ((next-code-pt-w-ind (one-of-with-index code))
+	       (next-code (car next-code-pt-w-ind))
+	       (next-index (cadr next-code-pt-w-ind))
+	       (rand-code-and-path (rand-code-point next-code))
+	       (rand-code (cadr rand-code-and-path))
+	       (path-to-code (car rand-code-and-path))
+	       (full-path (cons next-index path-to-code)))
+	  (cons full-path (cons rand-code nil))))))
+
+;; Slow and inefficient, but will do for now
+(defun code-point-with-type (desired-type code)
+  (let* ((prospective-point-and-path (rand-code-point code))
+	 (prospective-point (cadr prospective-point-and-path))
+	 (prospective-type (expr-type prospective-point)))
+    (if (eq desired-type prospective-type)
+	prospective-point-and-path
+      (code-point-with-type desired-type code))))
+
+(defun crossover (mother father)
+  (let* ((mother-point-and-path (rand-code-point mother))
+	 (mother-point (cadr mother-point-and-path))
+	 (mother-path (car mother-point-and-path))
+	 (mother-type (expr-type mother-point)))
+    (if (contains-type mother-type father)
+	(let* ((father-point-and-path (code-point-with-type (expr-type mother-point) father))
+	       (father-point (cadr father-point-and-path))
+	       (father-path (car father-point-and-path)))
+	  (swap-code-points mother-path mother-point mother father-path father-point father))
+      (crossover mother father))))
+
+(defun swap-point (parent-path parent new-point)
+  (if (null parent-path)
+      new-point
+    (set-nth parent (car parent-path) (swap-point (cdr parent-path) (nth (car parent-path) parent) new-point))))
+
+(defun swap-code-points (mother-path mother-point mother father-path father-point father)
+  (let ((mother-child (swap-point mother-path mother father-point))
+	(father-child (swap-point father-path father mother-point)))
+    (cons mother-child (cons father-child nil))))
